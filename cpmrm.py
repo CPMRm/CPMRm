985e6c6d6afc5faf567f1f2a12dca29059adbb30
CPMRm <rmcpmstore@gmail.com>
2025-05-02 21:51:51 +0800

    Update cpmrm.py

--- cpmrm.py
+++ cpmrm.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://chanryder.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://changryder.squareweb.app/api"
 
 class CPMRm:
     def __init__(self, access_key) -> None:
